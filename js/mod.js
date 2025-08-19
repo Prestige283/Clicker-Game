e1a996a1cc6b86534fad0434d1693521729e9d25
Prestige283 <sorratorn.sriburadej@gmail.com>
2025-08-18 21:08:01 -0700

    added stuff

---
+++ js/mod.js
@@ -0,0 +1,76 @@
+let modInfo = {
+	name: "Clicker Game",
+	author: "Prestige283",
+	id: "clickergame",
+	pointsName: "clicks",
+	modFiles: ["layers.js", "tree.js"],
+
+	discordName: "",
+	discordLink: "",
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	offlineLimit: 1,  // In hours
+}
+
+// Set your version in num and name
+let VERSION = {
+	num: "0.0",
+	name: "Nothing",
+}
+
+let changelog = ``
+
+let winText = `Congratulations! You have beaten Clicker Game!`
+
+// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
+// (The ones here are examples, all official functions are already taken care of)
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+
+function getStartPoints(){
+    return new Decimal(modInfo.initialStartPoints)
+}
+
+// Determines if it should show points/sec
+function canGenPoints(){
+	return true
+}
+
+// Calculate points/sec!
+function getPointGen() {
+	if(!canGenPoints())
+		return new Decimal(0)
+
+	let gain = new Decimal(0)
+	return gain
+}
+
+// You can add non-layer related variables that should to into "player" and be saved here, along with default values
+function addedPlayerData() { return {
+}}
+
+// Display extra things at the top of the page
+var displayThings = [
+]
+
+// Determines when the game "ends"
+function isEndgame() {
+	return player.points.gte(new Decimal("eee100000000"))
+}
+
+
+
+// Less important things beyond this point!
+
+// Style for the background, can be a function
+var backgroundStyle = {
+
+}
+
+// You can change this if you have things that can be messed up by long tick lengths
+function maxTickLength() {
+	return(3600) // Default is 1 hour which is just arbitrarily large
+}
+
+// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
+// you can cap their current resources with this.
+function fixOldSave(oldVersion){
+}
